cd1f01f14f0e4683bac19b96b567f46f957fde92
Selvedin <selvedinh@gmail.com>
2020-09-27 01:05:22 +0300

    fixing websocket

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const socket = io('/')
+const socket = io('/', { transports: ['websocket'] })
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
   host: '/',
